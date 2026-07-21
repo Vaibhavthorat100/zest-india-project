59a547036030617f06a73e3843f01e6085dc124f
Vaibhav Thorat <vaibhav.innonsh@gmail.com>
2026-07-21 23:58:00 +0530

    setup db context and configured DbSets

---
+++ StudentManagementAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using StudentManagementAPI.Models;
+using System.Reflection;
+
+namespace StudentManagementAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; } = null!;
+        public DbSet<ApplicationUser> Users { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
